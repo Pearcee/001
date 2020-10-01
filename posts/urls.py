e53b0853b9ab6b741578e77f2518a4cd9812a84a
Your Name <you@example.com>
2020-10-01 10:08:26 +0100

    comment

---
+++ posts/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+from django.views.generic.base import TemplateView
+from django.urls import path
+# from posts.views import robots_txt
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('', views.base_layout, name='base_layout'),
+]
+
+""" 
+from django.views.generic.simple import direct_to_template
+
+urlpatterns = patterns('',
+    ...
+    (r'^robots\.txt$', direct_to_template,
+     {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+)
+
+    path('robots.txt',views.robots, name='robots', content_type="text/plain"),
+ """
